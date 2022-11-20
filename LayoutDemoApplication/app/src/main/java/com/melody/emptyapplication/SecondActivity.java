b111941071a5d8f0d91f35d17c6c11727dc53136
yiqiwang <yiqiwang@tencent.com>
2022-11-20 11:56:45 +0800

    Add fragment demo

--- LayoutDemoApplication/app/src/main/java/com/melody/emptyapplication/SecondActivity.java
+++ LayoutDemoApplication/app/src/main/java/com/melody/emptyapplication/SecondActivity.java
@@ -8,8 +8,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import com.melody.emptyapplication.InterfaceFragment.TitleClickedEvent;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements TitleClickedEvent {
 
     private static final String TAG = "SecondActivity";
 
@@ -24,16 +26,26 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         Log.d("SecondActivity", "onCreate");
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, InterfaceFragment.class, null)
+                    .commit();
+        }
+
         Intent intent = getIntent();
         if (intent != null) {
             String input = intent.getStringExtra("input");
             TextView textView = findViewById(R.id.text_view_show);
-            textView.setText(input);
+            if (textView != null) {
+                textView.setText(input);
+            }
         }
     }
 
     /**
      * 界面初始化完成，事件监听等
+     * 由activity进行fragment的事件处理
      */
     @Override
     protected void onStart() {
@@ -48,6 +60,24 @@ public class SecondActivity extends AppCompatActivity {
                     SecondActivity.this.finish();
                 }
             });
+        }
+    }
+
+    @Override
+    public void onAttachFragment(Fragment fragment) {
+        super.onAttachFragment(fragment);
+        if (fragment instanceof InterfaceFragment) {
+            InterfaceFragment f = (InterfaceFragment)fragment;
+            f.setEvent(this);
+        }
+    }
+
+    @Override
+    public void onTitleClickListener(String title) {
+        Log.d(TAG, "onTitleClickListener " + title);
+        TextView textView = findViewById(R.id.text_view_show);
+        if (textView != null) {
+            textView.setText(title);
         }
     }
 }
